edaca7dc8034b503d71f1444d1b785564df0f7b7
QUESADA ZARAGOZA <marqueza@dsic.upv.es>
2020-01-05 12:47:07 +0100

    finished RoadEvaluator.java

--- pract4/RoadProblem/src/roadproblem/RoadCreator.java
+++ pract4/RoadProblem/src/roadproblem/RoadCreator.java
@@ -5,7 +5,9 @@ import java.util.Random;
 
 public class RoadCreator implements Creator<IntegerGenotype>{
 	public IntegerGenotype create() {
-		IntegerGenotype genotype = new IntegerGenotype(1, Datos.NUM_CUADRILLAS);
+		// the genotype is made up of 10 genes, one for workforce. 
+		// each gene is assigned to turn 1, 2 or 3
+		IntegerGenotype genotype = new IntegerGenotype(1, Datos.NUM_TURNOS);
 		genotype.init(new Random(), Datos.NUM_CUADRILLAS);
 		return genotype;
 	}
--- pract4/RoadProblem/src/roadproblem/RoadEvaluator.java
+++ pract4/RoadProblem/src/roadproblem/RoadEvaluator.java
@@ -2,19 +2,33 @@ package roadproblem;
 import java.util.ArrayList;
 import org.opt4j.core.Objectives;
 import org.opt4j.core.problem.Evaluator;
+import org.opt4j.core.Objective.Sign;
 
 public class RoadEvaluator  implements Evaluator<ArrayList<Integer>>{
 	@Override
 	public Objectives evaluate(ArrayList<Integer> fenotype) {
-		double result = 0.0;
+		int cost = 0;
+		int productivity = 0;
+		//stores how many wforces are assigned to each turn
+		int turnwforces[] = new int[3];
 		for(int id = 0; id < fenotype.size(); ++id) {
 			//get the values that represent this fenotype decoding it from the matrices in Data
+			turnwforces[fenotype.get(id) - 1] += 1;
+			cost += Datos.matrizCostes[id][fenotype.get(id) - 1];
+			productivity += Datos.matrizProductividad[id][fenotype.get(id) - 1];
+		}
+		//restriction: there has to be at least 3 wforces per turn
+		for(int i = 0; i < turnwforces.length; ++i) {
+			if(turnwforces[i] < 3){
+				cost = Integer.MAX_VALUE;
+				productivity = Integer.MIN_VALUE;
+				break;
+			}
 		}
 		Objectives objectives = new Objectives();
 		//if we are taking into consideration both cost and productivity, add 2 objectives
-		// Mirar ejemplo2 boletín
-		//objectives.add(...);
-		//objectives.add();
+		objectives.add("Coste-MIN", Sign.MIN, cost);
+		objectives.add("Productividad-MAX", Sign.MAX, productivity);
 		return objectives;
 	}
 
